c7192f7c8ff1c583807e24e54763f3dd42102734
huangwj <huangwj@infoearth.com>
2017-07-24 11:53:22 +0800

    修改日历

--- src/datepicker/datepicker.js
+++ src/datepicker/datepicker.js
@@ -618,16 +618,18 @@ angular.module('ui.bootstrap.datepicker', ['ui.bootstrap.dateparser', 'ui.bootst
       return attrs.templateUrl || 'uib/template/datepicker/datepicker.html';
     },
     scope: {
-      datepickerOptions: '=?'
+      datepickerOptions: '=?',
+      tag: "@"
     },
     require: ['uibDatepicker', '^ngModel'],
     restrict: 'A',
     controller: 'UibDatepickerController',
     controllerAs: 'datepicker',
     link: function(scope, element, attrs, ctrls) {
-      var datepickerCtrl = ctrls[0], ngModelCtrl = ctrls[1];
-
-      datepickerCtrl.init(ngModelCtrl);
+      attrs.$observe("tag",function(){
+        var datepickerCtrl = ctrls[0], ngModelCtrl = ctrls[1];
+        datepickerCtrl.init(ngModelCtrl);
+      });
     }
   };
 })
